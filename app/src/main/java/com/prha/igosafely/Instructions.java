39e6055dfa38fd21c0398bb92eb8dca922869752
akshayauti <akshayauti3260@gmail.com>
2019-10-02 15:44:27 +0530

    initial commit

---
+++ app/src/main/java/com/prha/igosafely/Instructions.java
@@ -0,0 +1,25 @@
+package com.prha.igosafely;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Instructions extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_instructions);
+		}
+	
+	public void back(View v) {
+		Intent i_back=new Intent(Instructions.this,MainActivity.class);
+		startActivity(i_back);
+			
+		}
+	
+	
+
+
+}
