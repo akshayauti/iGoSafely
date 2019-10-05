8bd2730339a736b18208ac1002ffd5524115b326
akshayauti <akshayauti3260@gmail.com>
2019-10-06 03:12:51 +0530

    added limits to registration

--- app/src/main/java/com/prha/igosafely/Register.java
+++ app/src/main/java/com/prha/igosafely/Register.java
@@ -2,6 +2,7 @@ package com.prha.igosafely;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -9,6 +10,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
 
 public class Register extends Activity {
 	
@@ -37,10 +40,12 @@ public class Register extends Activity {
 	
 	public void storeInDB(View v) {
 //		Toast.makeText(getApplicationContext(), "save started", Toast.LENGTH_LONG).show();
+		final SQLiteDatabase db;
+		db = openOrCreateDatabase("NumDB", Context.MODE_PRIVATE, null);
 		name = (EditText) this.findViewById(R.id.editText1);
 		number = (EditText) this.findViewById(R.id.editText2);
-		String str_name=name.getText().toString();
-		String str_number=number.getText().toString();
+		final String str_name=name.getText().toString();
+		final String str_number=number.getText().toString();
 		if(isEmpty(name)){
 			Toast.makeText(getApplicationContext(),"Name Should not be Empty!!",Toast.LENGTH_SHORT).show();
 			name.requestFocus();
@@ -50,26 +55,55 @@ public class Register extends Activity {
 			number.requestFocus();
 		}
 		else {
-			SQLiteDatabase db;
-			db = openOrCreateDatabase("NumDB", Context.MODE_PRIVATE, null);
+
 			//Toast.makeText(getApplicationContext(), "db created",Toast.LENGTH_LONG).show();
 
-			db.execSQL("CREATE TABLE IF NOT EXISTS details(id integer  primary key autoincrement,name VARCHAR ,number VARCHAR );");
+			db.execSQL("CREATE TABLE IF NOT EXISTS details( name VARCHAR ,number VARCHAR );");
 			//Toast.makeText(getApplicationContext(), "table created",Toast.LENGTH_LONG).show();
 
 			Cursor c = db.rawQuery("SELECT * FROM details", null);
+			c.moveToFirst();
 			if (c.getCount() < 2) {
-				db.execSQL("INSERT INTO details VALUES('" + str_name + "','" + str_number + "');");
+				db.execSQL("INSERT INTO details VALUES('"+ str_name + "','" + str_number + "');");
+				name.setText("");
+				number.setText("");
 				Toast.makeText(getApplicationContext(), "Successfully Saved", Toast.LENGTH_SHORT).show();
+				db.close();
 			} else {
-				db.execSQL("DELETE FROM details where id = LAST_INSERT_ID()-1");
+//				db = openOrCreateDatabase("NumDB", Context.MODE_PRIVATE, null);
+//				db.execSQL("DELETE FROM details where name=c.name");
+//				final SQLiteDatabase finalDb = db;
+				AlertDialog.Builder b=  new  AlertDialog.Builder(this)
+						.setTitle("Limit Exceed Delete Previous Numbers")
+						.setPositiveButton("OK",
+								new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog, int whichButton) {
+										db.execSQL("delete from details");
+										db.execSQL("INSERT INTO details VALUES('" + str_name + "','" + str_number + "');");
+										name.setText("");
+										number.setText("");
+										Toast.makeText(getApplicationContext(), "Successfully Saved", Toast.LENGTH_SHORT).show();
+										db.close();
+									}
+								}
+						)
+						.setNegativeButton("Cancel",
+								new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog, int whichButton) {
+										dialog.dismiss();
+									}
+								}
+						);
+						b.show();
 
-				db.execSQL("INSERT INTO details VALUES('" + str_name + "','" + str_number + "');");
-				Toast.makeText(getApplicationContext(), "Maximum Numbers limited reached. Previous numbers are replaced.", Toast.LENGTH_SHORT).show();
+
+
+
+//				Toast.makeText(getApplicationContext(), "Maximum Numbers limited reached. Previous numbers are replaced.", Toast.LENGTH_SHORT).show();
 			}
 
 
-			db.close();
+
 
 		}
 			
